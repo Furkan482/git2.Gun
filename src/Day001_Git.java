f922ac7e7608530337a114839cd4b4c3b8701008
Furkan Ozgan <ozganfurkan88@gmail.com>
2022-08-17 05:29:18 +0300

    zZ1

---
+++ src/Day001_Git.java
@@ -0,0 +1,6 @@
+public class Day001_Git {
+    public static void main(String[] args) {
+        System.out.println("Git 1. Deneme");
+        System.out.println("Git 02. Deneme");
+    }
+}
---
+++ src/Day002_Git.java
@@ -0,0 +1,30 @@
+public class Day002_Git {
+    public static void main(String[] args) {
+        System.out.println("Git 2. class");
+        /*
+        **************İnterview sorularına hazırlık önemli kısa notlar***************
+ 1-) git init --> Local repo olusturmak icin yani  .git ile klasörümüzün
+ içindeki dosyaları ilişkilendirmek için kullanılır
+
+ 2-) git add . --> Working space'den (yani yerel) dosyalarımı staging area'ya
+ (yani commitlemek için beklenen yer) gönderir
+
+ 3-) git status -->Working space'deki ve staging area'daki durumu gosterir
+
+ 4-) git commit -m "mesaj" --> Staging area'dan commit stora
+ dosyalarımı göndermek için kullanılır
+ (commit yani versiyon yani sürüm oluşturmuş olurum)
+
+ 5-) git push --> Uzak repo'ya(yani remote - GitHub)
+  göndermek için kullandıgımız kod
+ yalnız git push komutunu direkt kullanmak istersek 1 kez
+         git remote add origin adress
+         git push -u origin master
+
+NOT: Yukarıdaki iki komutu tek seferde kullandıktan sonra
+ ikinci commit'lerim için sadece git push kullanırız.
+ Not: git log --oneline --> Commitlerimdeki yani versiyonlarimdaki durumu gosterir
+
+  */
+    }
+}
