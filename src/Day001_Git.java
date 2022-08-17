18fa064f8a1b62d1ad6d47bc2f64145f03eb32c8
Furkan Ozgan <ozganfurkan88@gmail.com>
2022-08-17 18:12:32 +0300

    bvC

--- src/Day001_Git.java
+++ src/Day001_Git.java
@@ -2,5 +2,6 @@ public class Day001_Git {
     public static void main(String[] args) {
         System.out.println("Git 1. Deneme");
         System.out.println("Git 02. Deneme");
+        System.out.println("Git 03. Deneme");
     }
 }
