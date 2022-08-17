3edde3cda22fc37dd9b31e883680c42c94b7a5b5
Furkan482 <108580575+Furkan482@users.noreply.github.com>
2022-08-17 18:18:00 +0300

    Update Day001_Git.java

--- src/Day001_Git.java
+++ src/Day001_Git.java
@@ -1,8 +1,6 @@
 public class Day001_Git {
     public static void main(String[] args) {
-        System.out.println("Git 1. Deneme");
-        System.out.println("Git 02. Deneme");
-        System.out.println("Git 03. Deneme");
+    
         /*
         ******* KISAYOLLAR ********
         git --version
